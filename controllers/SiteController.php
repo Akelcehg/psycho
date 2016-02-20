2f3f06063dbfc3614a6c89a6abb2df3ee1255338
Akel <akelcheg@gmail.com>
2016-02-20 18:18:59 +0200

    upd

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -82,7 +82,6 @@ class SiteController extends Controller {
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
         }
-
         return $this->render('login', [
             'model' => $model,
         ]);
