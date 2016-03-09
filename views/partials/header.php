d9e48223b9b63e25a0994e598722575c290c2f3f
Akel <akelcheg@gmail.com>
2016-03-09 15:53:16 +0200

    upd

--- views/partials/header.php
+++ views/partials/header.php
@@ -9,33 +9,11 @@ use yii\helpers\Html;
     <!--NAVIGATION START-->
     <div class="navigation-bar">
         <div class="container">
+
             <div class="logo">
-                <a href="index.html"><img src="images/logo3.png" alt=""></a>
+                <a href="index.html"><img src="images/logo.png" alt=""></a>
             </div>
 
-
-            <!--<div class="cart">
-                <ul>
-                    <li>
-                        <div class="search-bar"><i class="fa fa-search"></i></div>
-                        <div class="search-box">
-                            <input type="text" class="input-block-level">
-                        </div>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#"
-                           href="/page.html">
-                            <i class="fa fa-briefcase"></i><span>€0</span>
-                        </a>
-                        <div class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-                            <p>0 items in shopping bag</p>
-                            <p class="color">Unfortunately, your shopping bag is emtpy.</p>
-                            <a href="#" class="return">Return to the Shop</a>
-                        </div>
-                    </li>
-                    <li><i class="fa fa-star"></i><span>3</span></li>
-                </ul>
-            </div>-->
             <div class="navigation">
                 <div class="navbar">
                     <div class="container">
@@ -48,14 +26,6 @@ use yii\helpers\Html;
                         <div class="nav-collapse collapse">
                             <ul>
                                 <li><a href="<?= Url::base() ?>">Главная</a></li>
-                                <li><a href="<?= Url::base() ?>/interesting">Интересное</a></li>
-                                <li><a href="<?= Url::base() ?>/article">Статьи</a></li>
-                                <li><a href="<?= Url::base() ?>/video">Видео</a></li>
-                                <li><a href="<?= Url::base() ?>/discussion">Форум</a></li>
-                                <li><a href="<?= Url::base() ?>/trainings">Тренинги</a></li>
-                                <li><a href="<?= Url::base() ?>/educational-institution">Школы</a></li>
-                                <li><a href="<?= Url::base() ?>/psychologists">Психологи</a></li>
-
                                 <?php if (Yii::$app->user->isGuest) { ?>
                                     <li>
                                         <a href="#">Кабинет</a>
@@ -68,7 +38,7 @@ use yii\helpers\Html;
 
                                     <li>
 
-                                        <a id="account" href="#">Мой профиль</a>
+                                        <a href="#">Мой профиль</a>
                                         <ul>
                                             <li><a href="account/profile">Profile</a></li>
                                             <li><a href="#">Account Setting</a></li>
@@ -83,8 +53,14 @@ use yii\helpers\Html;
                                         </ul>
 
                                     </li>
-
                                 <?php } ?>
+                                <li><a href="<?= Url::base() ?>/interesting">Интересное</a></li>
+                                <li><a href="<?= Url::base() ?>/article">Статьи</a></li>
+                                <li><a href="<?= Url::base() ?>/video">Видео</a></li>
+                                <li><a href="<?= Url::base() ?>/discussion">Форум</a></li>
+                                <li><a href="<?= Url::base() ?>/trainings">Тренинги</a></li>
+                                <li><a href="<?= Url::base() ?>/educational-institution">Школы</a></li>
+                                <li><a href="<?= Url::base() ?>/psychologists">Психологи</a></li>
 
                             </ul>
                         </div>
