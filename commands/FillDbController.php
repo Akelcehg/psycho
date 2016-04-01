de5a1a6a7966f710b7fa53df43a199ab12532aec
Akel <akelcheg@gmail.com>
2016-04-01 22:54:26 +0300

    console command

--- commands/FillDbController.php
+++ commands/FillDbController.php
@@ -6,13 +6,18 @@ use yii\console\Controller;
 
 class FillDbController extends Controller {
 
-    public function actionIndex($x, $y) {
-        echo $x . "\n";
-        echo $y . "\n";
+    public function actionIndex() {
+        return true;
     }
 
-    public function test(){
-
+    public function actionAll() {
+        echo "Dropping database\n";
+        //$this->stdout('C:\wamp64\bin\php\php5.6.16\php.exe yii migrate/down 1000');
+        exec('C:\wamp64\bin\php\php5.6.16\php.exe yii migrate/down 1000 --interactive=0');
+        echo "Initializing migrations\n";
+        exec('C:\wamp64\bin\php\php5.6.16\php.exe yii migrate --interactive=0');
+        echo "Updating Roles and adding default users\n";
+        exec('C:\wamp64\bin\php\php5.6.16\php.exe yii rbac/init');
     }
 
 }
--- modules/account/views/profile/index.php
+++ modules/account/views/profile/index.php
@@ -25,7 +25,7 @@ use yii\helpers\Html;
 
         <!--<img alt="Test image" id="testImage"/>-->
         <!--<img id="blah" src="#" alt="your image" />-->
-        <img alt="Test image" id="testImage" style="max-width:400px; max-height:400px;"/>
+        <img id="testImage" style="max-width:400px; max-height:400px;"/>
         <div id="previewWrap"></div>
         <!--<div id="previewWrap"></div>-->
 
